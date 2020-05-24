3f9b83dd0833cff319c0fc4f93f08506c7f877cd
dqn <suyarizzz@gmail.com>
2020-05-24 23:17:06 +0900

    feat: navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Home } from './components/pages/Home';
+import { Navigation } from './components/templetes/Navigation';
 
 export const App: React.FC = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/about">
-          <div>this is about page</div>
-        </Route>
-        <Route>
-          <div>404 Not Found</div>
-        </Route>
-      </Switch>
-    </Router>
+    <>
+      <Navigation />
+
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <div>this is about page</div>
+          </Route>
+          <Route>
+            <div>404 Not Found</div>
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 };
