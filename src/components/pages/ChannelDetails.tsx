262f0e4cf62f6a4066c6854dd17e5b55ff1ad5c1
dqn <suyarizzz@gmail.com>
2020-06-08 20:59:02 +0900

    feat: add pretty table

--- src/components/pages/ChannelDetails.tsx
+++ src/components/pages/ChannelDetails.tsx
@@ -5,6 +5,7 @@ import { getChannel, GetChannelResponse } from '@/api/channels';
 import { useParams } from 'react-router-dom';
 import { improveImageQuality } from '@/lib/youtube';
 import { getChats, GetChatsResponse } from '@/api/chats';
+import { PrettyTable, PrettyTableItem } from '../organisms/PrettyTable';
 
 export const ChannelDetails: React.FC = () => {
   const { id } = useParams();
@@ -20,6 +21,25 @@ export const ChannelDetails: React.FC = () => {
   if (!channel || !chats) {
     return <></>;
   }
+
+  const overviewItems: PrettyTableItem[] = [
+    {
+      title: 'Channel',
+      content: <a href={`https://www.youtube.com/channel/${channel.channelId}`}>YouTube</a>,
+    },
+    {
+      title: 'Sent chats',
+      content: channel.sentChatCount,
+    },
+    {
+      title: 'Recieved chats',
+      content: channel.receivedChatCount,
+    },
+    {
+      title: 'Updated at',
+      content: channel.updatedAt,
+    },
+  ];
 
   return (
     <div className="max-w-screen-lg mx-auto py-12">
@@ -37,34 +57,7 @@ export const ChannelDetails: React.FC = () => {
       <div className="flex flex-wrap mt-8">
         <div className="w-full">
           <span className="font-bold text-md ml-1">Overview</span>
-          <table className="w-full table-fixed">
-            <thead>
-              <tr>
-                <th className="w-1/2" />
-                <th className="w-2/2" />
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td className="border px-2 py-1">Channel</td>
-                <td className="border px-2 py-1">
-                  <a href={`https://www.youtube.com/channel/${channel.channelId}`}>YouTube</a>
-                </td>
-              </tr>
-              <tr className="bg-gray-100">
-                <td className="border px-2 py-1">Sent chats</td>
-                <td className="border px-2 py-1">{channel.sentChatCount}</td>
-              </tr>
-              <tr>
-                <td className="border px-2 py-1">Recieved chats</td>
-                <td className="border px-2 py-1">{channel.receivedChatCount}</td>
-              </tr>
-              <tr>
-                <td className="border px-2 py-1">Updated at</td>
-                <td className="border px-2 py-1">{channel.updatedAt}</td>
-              </tr>
-            </tbody>
-          </table>
+          <PrettyTable items={overviewItems} />
         </div>
         <div className="w-full mt-6">
           <span className="font-bold text-md ml-1">Recent chats</span>
