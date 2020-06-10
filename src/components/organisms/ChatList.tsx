0592f0afec8c2d4387d181b5923a450e25a6762c
dqn <suyarizzz@gmail.com>
2020-06-10 23:27:04 +0900

    feat: add link to channel on chat

--- src/components/organisms/ChatList.tsx
+++ src/components/organisms/ChatList.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ModeratorIcon from '@/assets/moderator.svg';
 import { GetChatsResponse } from '@/api/chats';
 import { Badge } from '@/types/models/badge';
+import { Link } from 'react-router-dom';
 
 export type Props = {
   chats: GetChatsResponse;
@@ -15,35 +16,34 @@ export const ChatList: React.FC<Props> = ({ chats }) => {
   return (
     <ul className="text-sm">
       {chats.map((chat, i) => (
-        <li key={i} className="border-t py-2 mx-2 my-auto">
-          <div className="flex">
-            <img src={chat.channel.imageUrl} className="rounded-full w-6 h-6" />
-            <div className="ml-2">
-              <span className="text-gray-600">{chat.timestamp}</span>
-              <span className="ml-2 my-auto inline">
+        <li key={i} className="border-t py-2 mx-2 my-auto flex">
+          <img src={chat.channel.imageUrl} className="rounded-full w-6 h-6" />
+          <div className="ml-2">
+            <span className="text-gray-600">{chat.timestamp}</span>
+            <span className="ml-2 my-auto">
+              <Link to={`/channels/${chat.channel.channelId}`}>
                 <span className={isModerator(chat.badges) ? 'moderator' : 'text-gray-600'}>
                   {chat.channel.name}
                 </span>
-                {isModerator(chat.badges) && <img src={ModeratorIcon} className="w-5 h-5 inline" />}
-                {chat.badges
-                  ?.filter((badge) => badge.badgeType === 'member')
-                  ?.map((badge) => (
-                    <img src={badge.imageUrl} className="w-5 h-5 ml-1 inline" />
-                  ))}
-              </span>
-
-              <div className="ml-2 inline">
-                {chat.messageElements.map((me, i) => (
-                  <span key={i} className="inline">
-                    {me.type === 'text' ? (
-                      <span className="text-gray-800">{me.text}</span>
-                    ) : (
-                      <img className="w-6 h-6 mx-1 inline" src={me.imageUrl} alt={me.label} />
-                    )}
-                  </span>
+              </Link>
+              {isModerator(chat.badges) && <img src={ModeratorIcon} className="w-5 h-5 inline" />}
+              {chat.badges
+                ?.filter((badge) => badge.badgeType === 'member')
+                ?.map((badge) => (
+                  <img src={badge.imageUrl} className="w-5 h-5 ml-1 inline" />
                 ))}
-              </div>
-            </div>
+            </span>
+            <span className="ml-2">
+              {chat.messageElements.map((me, i) => (
+                <span key={i}>
+                  {me.type === 'text' ? (
+                    <span className="text-gray-800">{me.text}</span>
+                  ) : (
+                    <img className="w-6 h-6 mx-1 inline" src={me.imageUrl} alt={me.label} />
+                  )}
+                </span>
+              ))}
+            </span>
           </div>
         </li>
       ))}
