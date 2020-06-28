6ba811215a407d5db6e8abbdcc182d4ef47c5204
dqn <suyarizzz@gmail.com>
2020-06-28 22:06:07 +0900

    chore: init with Next.js

--- src/api/client.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-import applyCaseMiddleware from 'axios-case-converter';
-
-export const client = applyCaseMiddleware(
-  axios.create({
-    baseURL: 'http://localhost:8080/api/',
-  }),
-);
--- src/types/models/badge.ts
+++
@@ -1,9 +0,0 @@
-export type Badge = {
-  id: number;
-  chatId: string;
-  badgeType: 'moderator' | 'member' | 'owner' | 'verified';
-  imageUrl: string;
-  label: string;
-  createdAt: string;
-  updatedAt: string;
-};
