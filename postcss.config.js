cc81d9c12bcde5f11903c22880b5a45db7686f79
dqn <suyarizzz@gmail.com>
2020-06-29 09:52:56 +0900

    chore: update configs

--- postcss.config.js
+++ postcss.config.js
@@ -1,35 +1,26 @@
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    'postcss-flexbugs-fixes',
-    [
-      'postcss-preset-env',
-      {
-        autoprefixer: {
-          flexbox: 'no-2009',
-        },
-        stage: 3,
-        features: {
-          'custom-properties': false,
-        },
+  plugins: {
+    tailwindcss: {},
+    'postcss-flexbugs-fixes': {},
+    'postcss-preset-env': {
+      autoprefixer: {
+        flexbox: 'no-2009',
       },
-    ],
+      stage: 3,
+      features: {
+        'custom-properties': false,
+      },
+    },
     ...(process.env.NODE_ENV === 'production'
-      ? [
-          [
-            '@fullhuman/postcss-purgecss',
-            {
-              content: ['./src/**/*.{js,jsx,ts,tsx}'],
-              defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-            },
-          ],
-        ]
-      : []),
-    [
-      'cssnano',
-      {
-        preset: 'default',
-      },
-    ],
-  ],
+      ? {
+          '@fullhuman/postcss-purgecss': {
+            content: ['./src/**/*.{js,jsx,ts,tsx}'],
+            defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+          },
+        }
+      : {}),
+    cssnano: {
+      preset: 'default',
+    },
+  },
 };
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: ['./src/**/*.html', './src/**/*.tsx'],
+  theme: {
+    extend: {
+      colors: {
+        owner: '#ffd600',
+        moderator: '#5e84f1',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
