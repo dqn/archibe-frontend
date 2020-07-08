97b245f5aa9a87e1aeea9289710089e7bdf4ae73
dqn <suyarizzz@gmail.com>
2020-07-09 01:21:46 +0900

    fix: modify api endpoint for production

--- src/api/client.ts
+++ src/api/client.ts
@@ -5,7 +5,7 @@ export const client = applyCaseMiddleware(
   axios.create({
     ...(process.env.NODE_ENV !== 'production'
       ? { baseURL: `http://localhost:${process.env.PORT ?? 3000}/` }
-      : {}),
+      : { baseURL: process.env.ENDPOINT }),
   }),
 );
 
