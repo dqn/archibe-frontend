bdfa87a7ad7396ea2d8426e05a974ef1865441d4
dqn <suyarizzz@gmail.com>
2020-07-08 19:56:22 +0900

    fix: switch axios config by environment

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,12 @@
 import axios from 'axios';
 import applyCaseMiddleware from 'axios-case-converter';
 
-export const client = applyCaseMiddleware(axios.create());
+export const client = applyCaseMiddleware(
+  axios.create({
+    ...(process.env.NODE_ENV !== 'production'
+      ? { baseURL: `http://localhost:${process.env.PORT ?? 3000}/` }
+      : {}),
+  }),
+);
 
 client.interceptors.response.use((response) => response.data);
